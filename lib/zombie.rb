e0b69bb99fe0d0a82bc5e574b0cefef4227a1330
drborges <drborges.cic@gmail.com>
2013-11-03 00:04:34 -0400

    level 2: change matcher usage

--- lib/zombie.rb
+++ lib/zombie.rb
@@ -1,7 +1,12 @@
 class Zombie
-  attr_accessor :tweets
+  attr_accessor :tweets, :iq
 
   def initialize(options = {})
     @tweets = options[:tweets]
+    @iq = 0
+  end
+
+  def eat_brains
+    @iq += 3
   end
 end
--- spec/lib/zombie_spec.rb
+++ spec/lib/zombie_spec.rb
@@ -7,4 +7,9 @@ describe Zombie do
     zombie = Zombie.new(tweets: [tweet])
     zombie.tweets.should include(tweet)
   end
+
+  it 'gains 3 IQ points by eating brains' do
+    zombie = Zombie.new
+    expect { zombie.eat_brains }.to change { zombie.iq }.by(3)
+  end
 end
